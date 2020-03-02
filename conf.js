705c8e6d5c6e4605ef22e0896bbecd13a0979cfa
anserme <lczr1026673211@gmail.com>
2020-03-02 12:00:28 -0600

    add node

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ jsproxy_config({
   // 当前配置的版本（记录在日志中，用于排查问题）
   // 每次修改配置，该值需要增加，否则不会生效。
   // 默认每隔 5 分钟自动下载配置，若想立即验证，可通过隐私模式访问。
-  ver: '110',
+  ver: '111',
 
   // 通过 CDN 加速常用网站的静态资源（实验中）
   static_boost: {
@@ -24,6 +24,12 @@ jsproxy_config({
       label: '演示服务-新加坡节点',
       lines: {
         'node-aliyun-sg.etherdream.com:8443': 1,
+      },
+    },
+    'prod-us': {
+      label: '美国',
+      lines: {
+        'ao3.dreamback.com:8443': 1,
       },
     },
     'mysite': {
@@ -53,7 +59,7 @@ jsproxy_config({
   /**
    * 默认节点
    */
-  node_default: 'mysite',
+  node_default: 'prod-us',
   // node_default: /jsproxy-demo\.\w+$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
